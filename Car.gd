7cabdc5f8c9afe582a7846de5ef226c4fb0a4c9c
shiroxkuro77 <103314294+shiroxkuro77@users.noreply.github.com>
2022-12-12 23:56:47 +0800

    Add lightning and fixed movement speed

--- Car.gd
+++ Car.gd
@@ -3,7 +3,8 @@ extends RigidBody2D
 var initial_pos = position.x
 var moving = false
 var unitBlock = 16
-
+var time = 0
+const TIME_PERIOD = 0.1
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -16,18 +17,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if moving:
-		#AnimatedSprite.play("go_right")
-		position.x += unitBlock
-		if position.x >= initial_pos + (3 * unitBlock):
-			stop_moving()
-
-func _fixed_process():
-	if moving:
-		#AnimatedSprite.play("go_right")
-		position.x += unitBlock
-		if position.x >= initial_pos + (3 * unitBlock):
-			stop_moving()
+	time += delta
+	if time > TIME_PERIOD:
+		if moving:
+			#AnimatedSprite.play("go_right")
+			position.x += unitBlock
+			if position.x >= initial_pos + (3 * unitBlock):
+				stop_moving()
+		time = 0
 
 func stop_moving():
 	moving = false
--- Cloud.gd
+++ Cloud.gd
@@ -3,18 +3,24 @@ extends Node2D
 signal lightning
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+var has_lightning = false
 var unitBlock = 16
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimatedSprite.hide()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if has_lightning and $LightningTimer.is_stopped():
+		emit_signal("lightning")
+		$AnimatedSprite.stop()
+		$AnimatedSprite.hide()
+		has_lightning = false
 	
 	if Input.is_action_just_pressed("move_left"):
 		print(position.x)
@@ -25,4 +31,7 @@ func _process(delta):
 		if position.x < OS.get_screen_size().x - 32:
 			position.x += unitBlock
 	elif Input.is_action_just_pressed("execute"):
-		emit_signal("lightning")
+		$AnimatedSprite.show()
+		$LightningTimer.start()
+		$AnimatedSprite.play()
+		has_lightning = true
