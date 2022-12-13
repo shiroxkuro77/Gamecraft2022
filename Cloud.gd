f50f668c8c89c16a36c1c6fe2c745eed8c58ca14
shiroxkuro77 <103314294+shiroxkuro77@users.noreply.github.com>
2022-12-13 22:58:56 +0800

    Add tree cloud and seed bullet

--- Cloud.gd
+++ Cloud.gd
@@ -1,19 +1,11 @@
 extends Node2D
 
-signal lightning
+class_name Cloud
 
 # Declare member variables here. Examples:
-
-var has_lightning = false
 var unitBlock = 16
-
 var time = 0
 const TIME_PERIOD = 0.1
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$AnimatedSprite.hide()
-	pass # Replace with function body.
 
 func _timer(delta):
 	time += delta
@@ -23,14 +15,7 @@ func _timer(delta):
 	return false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
-	if has_lightning and $LightningTimer.is_stopped():
-		emit_signal("lightning")
-		$AnimatedSprite.stop()
-		$AnimatedSprite.hide()
-		has_lightning = false
-	
+func _process(delta):		
 	if Input.is_action_pressed("move_left"):
 		print(position.x)
 		if position.x > 32:
@@ -41,8 +26,3 @@ func _process(delta):
 		if position.x < OS.get_screen_size().x - 32:
 			if _timer(delta):
 				position.x += unitBlock
-	elif Input.is_action_just_pressed("execute"):
-		$AnimatedSprite.show()
-		$LightningTimer.start()
-		$AnimatedSprite.play()
-		has_lightning = true
---
+++ LightningCloud.gd
@@ -0,0 +1,34 @@
+extends Cloud
+
+class_name LightningCloud
+
+signal lightning
+
+# Declare member variables here. Examples:
+var has_lightning = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.hide()
+	pass # Replace with function body.
+
+func _timer(delta):
+	time += delta
+	if time > TIME_PERIOD:
+		time = 0
+		return true
+	return false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if has_lightning and $LightningTimer.is_stopped():
+		emit_signal("lightning")
+		$AnimatedSprite.stop()
+		$AnimatedSprite.hide()
+		has_lightning = false
+	
+	elif Input.is_action_just_pressed("execute"):
+		$AnimatedSprite.show()
+		$LightningTimer.start()
+		$AnimatedSprite.play()
+		has_lightning = true
