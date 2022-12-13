eb35ad7fd3764b8d501ad574aa910a0e4844ccbd
shiroxkuro77 <103314294+shiroxkuro77@users.noreply.github.com>
2022-12-13 15:38:23 +0800

    Add ray cast to detect ground and car will fall if no ground

--- Car.gd
+++ Car.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+onready var ground_ray = get_node("GroundRay2D");
 var initial_pos = position.x
 var moving = false
 var unitBlock = 16
@@ -12,17 +13,23 @@ const TIME_PERIOD = 0.1
 func _ready():
 	pass # Replace with function body.
 
+func _timer(delta):
+	time += delta
+	if time > TIME_PERIOD:
+		time = 0
+		return true
+	return false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	time += delta
-	if time > TIME_PERIOD:
+	if _timer(delta):
 		if moving:
 			#AnimatedSprite.play("go_right")
 			position.x += unitBlock
 			if position.x >= initial_pos + (moveSteps * unitBlock):
 				stop_moving()
-		time = 0
+		if not ground_ray.is_colliding():
+			position.y += unitBlock
 
 func stop_moving():
 	moving = false
