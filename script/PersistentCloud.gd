229ab366b7d885ca3691bc34b2d2f86c78dc18e3
shiroxkuro77 <103314294+shiroxkuro77@users.noreply.github.com>
2022-12-14 23:15:43 +0800

    Add tornado artwork

--- script/PersistentCloud.gd
+++ script/PersistentCloud.gd
@@ -12,7 +12,30 @@ func _ready():
 		if not child == LightningNode:
 			remove_child(child)
 			
-func _switchNode():
+func _switchNode():--- Debugging process started ---
+Godot Engine v3.5.1.stable.official.6fed1ffa3 - https://godotengine.org
+OpenGL ES 3.0 Renderer: NVIDIA GeForce GTX 1070 Ti/PCIe/SSE2
+Async. shader compilation: OFF
+ 
+--- Debugging process stopped ---
+Switch Scene Tab
+Switch Scene Tab
+Switch Scene Tab
+Switch Scene Tab
+ res://LightningCloud.gd:4 - Parse Error: Unique global class "LightningCloud" already exists at path: res://script/LightningCloud.gd
+Switch Scene Tab
+Switch Scene Tab
+Switch Scene Tab
+Switch Scene Tab
+Switch Scene Tab
+Switch Scene Tab
+Switch Scene Tab
+ scene/main/node.cpp:1753 - Condition "!common_parent" is true. Returned: NodePath()
+ scene/main/node.cpp:1753 - Condition "!common_parent" is true. Returned: NodePath()
+Paste Node(s)
+Reparent Node
+Cut Node(s)
+
 	if currNode == LightningNode:
 		add_child(TreeNode)
 		remove_child(currNode)
