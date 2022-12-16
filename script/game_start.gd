443645a2fc8d2de38e041fb67d08290415ad54dd
JJiahui <jiangjiahui2048@gmail.com>
2022-12-16 22:05:43 +0800

    created game start scene

--- script/game_start.gd
+++ script/game_start.gd
@@ -1,5 +1,11 @@
 extends Node
 
+
+func _ready():
+	var screen_size = OS.get_screen_size()
+	var window_size = OS.get_window_size()
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
+	
+
 func _on_start_game_button_pressed():
 	get_tree().change_scene("res://scene/instructions_level_1.tscn")
-	pass # Replace with function body.
