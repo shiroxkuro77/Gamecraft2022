cd2b5965b5819b5a546d7caf1d07b45cb65275fd
JJiahui <jiangjiahui2048@gmail.com>
2022-12-17 02:13:10 +0800

    changed press enter to press space

--- script/Main.gd
+++ script/Main.gd
@@ -11,14 +11,14 @@ func _ready():
 #		$Car.trigger_move_right()
 
 
-
-
-func _on_Button_pressed():
-	if name == "instructions_level_1":
-		get_tree().change_scene("res://scene/Level1.tscn")
-	
-	if name == "instructions_level_2":
-		get_tree().change_scene("res://scene/Level2.tscn")
-		
-	if name == "instructions_level_3":
-		get_tree().change_scene("res://scene/Level3.tscn")
+func _process(delta):
+	if name.begins_with("instructions") and Input.is_action_just_released("execute"):
+		if name == "instructions_level_1":
+			get_tree().change_scene("res://scene/Level1.tscn")
+			return
+		if name == "instructions_level_2":
+			get_tree().change_scene("res://scene/Level2.tscn")
+			return
+		if name == "instructions_level_3":
+			get_tree().change_scene("res://scene/Level3.tscn")
+			return
